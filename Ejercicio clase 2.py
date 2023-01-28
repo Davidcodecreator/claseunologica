553dadd0a0676e13d62a4889c0a189b35a75c2fd
Davidcodecreator <123257817+Davidcodecreator@users.noreply.github.com>
2023-01-28 10:39:41 -0600

    Add files via upload

---
+++ Ejercicio clase 2.py
@@ -0,0 +1,13 @@
+# Entradas
+import math
+print('Calcular el área y perimetro de un circulo')
+pi = 3.1416
+radio = float(input('Inserta el radio:'))
+área = 0
+perimetro = 0
+# Procesos
+área = (pi*radio)**2
+perimetro = 2*pi*radio
+# Salidas
+print("El área del circulo es:", área)
+print("El perimetro del circulo es:", perimetro)
