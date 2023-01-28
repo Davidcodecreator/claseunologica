be592eff71cff3fc7bd0f5a091f6184e169da6f8
Davidcodecreator <123257817+Davidcodecreator@users.noreply.github.com>
2023-01-28 10:39:21 -0600

    Add files via upload

---
+++ Ejercicio clase 1.py
@@ -0,0 +1,12 @@
+# Entradas
+calificación_1 = float(input('Escribe una calificación: '))
+calificación_2 = float(input('Escribe otra calificación:'))
+calificación_3 = float(input('Escribe la última calificación:'))
+promedio = 0
+# Procesos
+promedio = (calificación_1 + calificación_2 + calificación_3)/3
+
+# Salidas
+print("El promedio es:", promedio)
+
+
