3d3213f1b65140de969b3e60382b6417d67351a1
Davidcodecreator <123257817+Davidcodecreator@users.noreply.github.com>
2023-01-28 13:51:58 -0600

    Add files via upload

---
+++ Ejercicio clase 3.py
@@ -0,0 +1,7 @@
+# Calcular la edad de una persona
+año_nacimiento = int(input('Insertar la edad de nacimiento : '))
+año = int(input("Insertar el año actual para el cálculo : "))
+edad = año - año_nacimiento
+print("La edad en años de la persona es:", edad)
+
+
