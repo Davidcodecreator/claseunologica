568d45ac4b2c2736955d0404c66c52097923c16e
Davidcodecreator <123257817+Davidcodecreator@users.noreply.github.com>
2023-01-28 08:57:58 -0600

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,34 @@
+# variables
+número1=10
+número2=20
+suma= 0
+resta= 0
+potencia1 = 0
+potencia2=0
+raízcuadrada1=0
+raízcuadrada2=0
+raízcúbica=0
+
+# procesos
+suma = número2 + número2
+resta = número1 - número2
+
+potencia1= número1 ** número2
+potencia2= pow(número1, número2)
+raízcuadrada1= pow(número1,2)
+raízcuadrada2= pow(número2,2)
+raízcúbica= pow(número1,3)
+
+#salida de datos 
+print("La suma es igual a", suma)
+print("La resta es igual a", resta)
+
+print("La potencia 1 =", potencia1)
+print("La potencia 2 =", potencia2)
+
+print("La suma es igual a {suma}")
+print("La suma es igual a" + str(suma))
+print("La raíz cuadrada del numero 1 =", raízcuadrada1)
+print("La raíz caundrada del numero 2 =",raízcuadrada2)
+print("La raíz cúbica =", raízcúbica)
+
